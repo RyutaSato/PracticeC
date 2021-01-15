e7c0b0be13660d8829889228f95cdb3311b4ce6e
SatuN <62854047+RyutaSato@users.noreply.github.com>
2021-01-15 15:22:25 +0900

    Add files via upload
    func menu, jump are made.
    inputData, main are changed.

--- r19.c
+++ r19.c
@@ -4,76 +4,117 @@
 #include <stdlib.h>
 #include <string.h>
 #define fileName "R19_in.txt"
-void inputData(PersonalInfo *);
-void serchId(PersonalInfo *);
-void serchName(PersonalInfo *);
-void outputData(PersonalInfo *);
-
+#define SetSerchBMI 25
 typedef struct{
     double height;
     double weight;
-} PersonalInfo;
+} Status;
 
-typedef struct member{
-    struct member *next;
+typedef struct personalInfo{
+    struct personalInfo *next;
     int id;
     char name[30];
-    PersonalInfo personalInfo;
-}MEMBER;
+    Status status;
+}PersonalInfo;
 
+int menu(void);
+void inputData(PersonalInfo *data);
+void searchId(PersonalInfo *data);
+void searchName(PersonalInfo *data);
+void outputAllData(PersonalInfo *data);
+void bmi25AboveData(PersonalInfo *data);
 /*-------------------------------------------*/
 int main(void){
-    PersonalInfo endPoint = {NULL};
-    PersonalInfo* strPoint = &endPoint;
-    inputData(strPoint);
-    serchId(strPoint);
-    serchName(strPoint);
-    outputData(strPoint);
+    jump(menu());
     return 0;
 }
 /*-------------------------------------------*/
+
+void jump(int select){
+    PersonalInfo endPoint = {NULL};
+    PersonalInfo *strPoint = &endPoint;
+    inputData(strPoint);
+    switch(select){
+        case 1:
+            outputAllData();
+            break;
+        case 2:
+            searchId(strPoint);
+            break;
+        case 3:
+            searchName(strPoint);
+            break;
+        case 4:
+            bmi25AboveData(PersonalInfo *data);
+            break;
+        default:
+            free(strPoint);
+            free(endPoint);
+            jump(menu());
+            break;
+    }
+    
+}
+int menu(void){
+    int select;
+    do{
+        printf("-------Menu---------\n");
+        printf("1:Display all data\n");
+        printf("2:Search ID\n");
+        printf("3:Search NAME\n");
+        printf("4:Display data for BMI 25 and above\n");
+        printf("SELECT:")
+        
+    }while(scanf("%d",&select) != 0);
+    return select;
+}
 
 void inputData(PersonalInfo *data){
     FILE *fp;
     char str[200];
     PersonalInfo *tmp={NULL},*keep;
-    fp=fopen(fileName,"r");
+    
+    fp=fopen("R19_in.txt","r");
     if(fp==NULL){
         printf("ERROR EXIT(1)\n");
-        exit(1)
+        exit(1);
     }
-    fgets(str,200,fp);
-    do{
-        if(tmp!=NULL){ //maybe error
-            PersonalInfo *allocatedSpace=(PersonalInfo *)calloc(1,sizeof(PersonalInfo));
-            if(allocatedSpace==NULL){
-                printf("ERROR EXIT(2)\n");
-                exit(2);
-            }
-            allocatedSpace=tmp;
-            for(keep=start;keep!=NULL;keep=keep->next){
-                if(allocatedSpace->id < allocatedSpace->next->id){
-                    allocatedSpace->next=keep->next;
-                    keep->next=allocatedSpace;
-                    break;
-                }
-            }
-            if(keep->next==NULL){
-                keep->next=keep;
-                keep->next=NULL;
-            }
-            
+    
+    while(fscanf(fp,"%d %s %lf %lf",tmp->id,tmp->name,tmp->status.height,tmp->status.weight)!=EOF){
+        
+        PersonalInfo *allocatedSpace=(PersonalInfo *)calloc(1,sizeof(PersonalInfo));
+        if(allocatedSpace==NULL){
+            printf("ERROR EXIT(2)\n");
+            exit(2);
         }
-    }while(fscanf(fp,"%d %s %lf %lf",tmp->id,tmp->name,tmp->personalInfo->height,tmp->personalInfo->weight)==0);
-
+        allocatedSpace=tmp;
+        for(keep=data;keep!=NULL;keep=keep->next){
+            if(allocatedSpace->id < allocatedSpace->next->id){
+                allocatedSpace->next=keep->next;
+                keep->next=allocatedSpace;
+                break;
+            }
+        }
+        if(keep->next==NULL){
+            keep->next=keep;
+            keep->next=NULL;
+        }
+    }
+    printf("line80\n");
     fclose(fp);
-}
-void serchId(PersonalInfo *){
     
 }
-void serchName(PersonalInfo *){
+void searchId(PersonalInfo *data){
+    
     
 }
-void outputData(PersonalInfo *){
+void searchName(PersonalInfo *data){
     
+}
+void outputAllData(PersonalInfo *data){
+    PersonalInfo *tmp=data;
+    while(tmp->next !=NULL){
+        printf("%5d %s %5.1f %5.1f\n",tmp->id,tmp->name,tmp->status.height,tmp->status.weight);
+        tmp=tmp->next;
+    }
 }
